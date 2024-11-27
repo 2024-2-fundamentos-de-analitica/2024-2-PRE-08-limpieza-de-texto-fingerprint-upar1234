9d69f6c77b259c1394b22a72f4befc0ff1bb9ed4
upar1234 <upar1234@gmail.com>
2024-11-27 09:45:03 -0500

    a

--- homework/clean_delta.py
+++ homework/clean_delta.py
@@ -24,11 +24,11 @@ def create_key(df):
     df["key"] = df["key"].str.strip()
 
     # Convierta el texto a minúsculas
-    df["key"] = df["key"].str.lower() 
+    df["key"] = df["key"].str.lower()
 
     # Transforme palabras que pueden (o no) contener guiones por su
     # version sin guion.
-    df["key"] = df["key"].str.replace("-", "")    
+    df["key"] = df["key"].str.replace("-", "")
 
     # Remueva puntuación y caracteres de control
     df["key"] = df["key"].str.translate(
@@ -46,7 +46,7 @@ def create_key(df):
     df["key"] = df["key"].apply(lambda x: sorted(set(x)))
 
     # Convierta la lista de tokens a una cadena de texto separada por espacios
-    df["key"] = df["key"].str.join(" ") 
+    df["key"] = df["key"].str.join(" ")
 
     return df
 
@@ -55,22 +55,23 @@ def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
     df = df.copy()
-    df_orig = df.copy()
+    # df_orig = df.copy()
 
     # Ordene el dataframe por 'key' y 'text'
-    df = df.sort_values(by=["key", "text"], ascending=[True, True])    
+    df = df.sort_values(by=["key", "text"], ascending=[True, True])
 
     # Seleccione la primera fila de cada grupo de 'key'
     keys = df.drop_duplicates(subset="key", keep="first")
 
     # Cree un diccionario con 'key' como clave y 'text' como valor
     # [(adhoc queri, AD-HOC QUERIES), (agricultur product, AGRICULTURAL PRODUCTION), ... ]
-    key_dict = dict(zip(keys["key"], keys["text"]))    
+    key_dict = dict(zip(keys["key"], keys["text"]))
 
     # Cree la columna 'cleaned' usando el diccionario
-    df_orig["cleaned"] = df_orig["key"].map(key_dict)
+    # df_orig["cleaned"] = df_orig["key"].map(key_dict)
+    df["cleaned"] = df["key"].map(key_dict)
 
-    return df_orig
+    return df
 
 
 def save_data(df, output_file):
@@ -91,19 +92,11 @@ def main(input_file, output_file):
     df = load_data(input_file)
     df = create_key(df)
     df = generate_cleaned_column(df)
-    df.to_csv("files/output/test.csv", index=False)
-    save_data(df, output_file)
-
 
 
 
 if __name__ == "__main__":
     main(
         input_file="files/input/input.txt",
-        output_file="files/output/output.txt",
+        output_file="files/output.txt",
     )
-
-
-
-
-    
